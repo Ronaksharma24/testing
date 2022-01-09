5c6cb623203081919dfbf99648bf56f65747d93b
Ronaksharma24 <shrm_ronak@yahoo.co.uk>
2022-01-09 11:47:24 +0000

    change in first programme

--- mygit.java
+++ mygit.java
@@ -1,6 +1,11 @@
 public class mygit {
 
     public  static void main (String [] args){
-        System.out.println("hello new repository");
+        int a=10;
+        int b=10;
+        int result;
+        result = a*b;
+
+        System.out.println("multiplication is: " + result);
     }
 }
